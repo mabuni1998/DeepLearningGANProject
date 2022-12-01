82b78556b74e0b40883b7227665ac2f58b532506
ChristianBangGribsvad <jcbg1997@gmail.com>
2022-12-01 15:50:06 +0100

    'GAN_cifar10.py'

--- jobfile.sh
+++ jobfile.sh
@@ -12,7 +12,8 @@
 source /dtu/sw/dcc/dcc-sw.bash
 module load cuda/11.5
 module load cudnn/v8.3.2.44-prod-cuda-11.5
-source ~/DeepLearning/pytorch_env/bin/activate
+source ~/Deep_Learning/pytorch_env/bin/activate
 echo $LSB_JOBINDEX
 #Run script run_gpu.py with inputs
-python3 GAN_mnist.py
+python3 GAN_cifar10.py
+
--- push.sh
+++ push.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
+#!/bin/sh
 
-scp -i ~/.ssh/gbar jobfile.sh GAN_mnist.py WGAN_mnist_spectralnorm.py WGAN_mnist_weightclipping.py FID_score.py s173981@transfer.gbar.dtu.dk:~/DeepLearning/
+scp GAN_cifar10.py jobfile.sh FID_score.py s174477@transfer.gbar.dtu.dk:~/DeepLearning/
